47b75c689509b835ef9f170440aa84d318a5a3e2
Monower Sadaf <monowersadaf@gmail.com>
2021-04-21 20:30:33 +0600

    added the pages folder

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Moviezone</a>
+  <a class="navbar-brand" href="index.php">Moviezone</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,18 +7,18 @@
   <div class="collapse navbar-collapse" id="navbarColor02">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="#">Home
+        <a class="nav-link" href="index.php">Home
           <span class="sr-only">(current)</span>
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
+        <a class="nav-link" href="./pages/about.php">About</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Categories</a>
         <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">Bangla</a>
-          <a class="dropdown-item" href="#">English</a>
+          <a class="dropdown-item" href="./pages/bangla.php">Bangla</a>
+          <a class="dropdown-item" href="./pages/english.php">English</a>
         </div>
       </li>
     </ul>
