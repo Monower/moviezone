90148aa8a98df0bf9b315ef1308029b43107e18d
Monower Sadaf <monowersadaf@gmail.com>
2021-04-21 20:09:45 +0600

    this is first commit for moviezone

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include './templates/header.php' ?>
+<?php include './templates/navbar.php' ?>
+<div class="center" >
+    <h2>Welcome to movie zone</h2>
+</div>
+<?php include './templates/footer.php' ?>
---
+++ templates/navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Moviezone</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home
+          <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Categories</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Bangla</a>
+          <a class="dropdown-item" href="#">English</a>
+        </div>
+      </li>
+    </ul>
+
+  </div>
+</nav>
