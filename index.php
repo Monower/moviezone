9b3aa19dde993b0df847dc09f8fcb51b2913a2d1
Monower Sadaf <monowersadaf@gmail.com>
2021-04-22 22:32:16 +0600

    few minor changes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include './templates/index_header.php' ?>
 <?php include './templates/navbar.php' ?>
 <div class="center" >
-    <h2>Welcome to movie zone</h2>
+    <h2>Welcome to MovieZone</h2>
 </div>
 <?php include './templates/footer.php' ?>
--- pages/about.php
+++ pages/about.php
@@ -1,7 +1,8 @@
 <?php include '../templates/pages_header.php' ?>
 <?php include '../templates/pages_navbar.php' ?>
 <div class="center" >
-    <h2>This is about</h2>
+    <h2>About us</h2>
+    <p>MovieZone is a blogpost website.<br> Here you can get information about various kinds of movies.</p>
 </div>
 
 <?php include '../templates/footer.php' ?>
