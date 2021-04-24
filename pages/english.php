3b17f36bec121308861f9d1c7308caa6a57eed19
Monower Sadaf <monowersadaf@gmail.com>
2021-04-24 10:10:19 +0600

    modified english.php

--- pages/english.php
+++ pages/english.php
@@ -1,7 +1,32 @@
 <?php include '../templates/pages_header.php' ?>
 <?php include '../templates/pages_navbar.php' ?>
+<?php 
+include '../db/dbconnect.php';
+$conn=$obj->open();
+
+?>
 <div class="center" >
     <h2>This is English</h2>
+    <?php 
+    
+    $sql="SELECT name FROM movies where category='English'";
+    $result=$conn->query($sql);
+    if($result==true)
+    {
+        while ($row=$result->fetch_assoc()) {
+            echo "Name:".$row['name']."<br>";
+
+        }
+    }
+    else {
+        echo $conn->error;
+    }
+
+
+
+
+
+?>
 </div>
 
 <?php include '../templates/footer.php' ?>
